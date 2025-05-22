6150e2a318afbf0ee9035de821f7fc38b9ed1412
Rezwan Ahmed <dewanrezwanahmed@gmail.com>
2025-05-22 12:16:41 +0600

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,28 @@
+import { useRef } from "react";
+
+
+const App = () => {
+
+  let APIdata = useRef(null);
+  let myPTag = useRef();
+
+  const fetchData = async() => {
+    const response = await fetch("https://dummyjson.com/products")
+    APIdata.current = await response.json();
+  }
+
+  const showData = () => {
+    myPTag.current.innerText = JSON.stringify(APIdata.current);
+  }
+
+
+  return (
+    <div>
+      <p ref={myPTag}></p>
+      <button onClick={fetchData}>Call API</button>
+      <button onClick={showData}>Show Data</button>
+    </div>
+  );
+};
+
+export default App;
---
+++ src/component/Hero.jsx
@@ -0,0 +1,11 @@
+
+
+const Hero = (props) => {
+    return (
+        <div>
+            <button onClick={props.clickedBtn}>Click Me</button>
+        </div>
+    );
+};
+
+export default Hero;
